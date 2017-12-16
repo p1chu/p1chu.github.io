b13ca1cdb7a860ed6e138d207d5a6f5f79d90c32
p1chu <p1chu@users.noreply.github.com>
2017-12-15 17:58:05 -0800

    Deleted Open Time Setter

--- js/receive.js
+++ js/receive.js
@@ -84,68 +84,6 @@ function setTheHour (data) {
 
 }
 
-function setTheOpenHour (data) {
-	
-	currentValue = parseInt(data);
-	
-	// I needed 12 ranges of 80 to get my 12 hours, so I reset the potentiometer max to 960 in NTK (960/12 = 80)
-	var range = Math.floor(currentValue/80)
-	
-	switch (range) {
-		case 0:
-			//0-79 : 1 o'clock
-			hour = 1;
-			break;
-		case 1:
-			//80-159 : 2 o'clock
-			hour = 2;
-			break;
-		case 2:
-			//160-239 : 3 o'clock
-			hour = 3;
-			break;
-		case 3:
-			//240-319 : 4 o'clock
-			hour = 4;
-			break;
-		case 4:
-			//320-399 : 5 o'clock
-			hour = 5;
-			break;
-		case 5:
-			//400-479 : 6 o'clock
-			hour = 6;
-			break;
-		case 6:
-			//480-559 : 7 o'clock
-			hour = 7;
-			break;
-		case 7:
-			//560-639 : 8 o'clock
-			hour = 8;
-			break;
-		case 8:
-			//640-719 : 9 o'clock
-			hour = 9;
-			break;
-		case 9:
-			//720-799 : 10 o'clock
-			hour = 10;
-			break;
-		case 10:
-			//800-879 : 11 o'clock
-			hour = 11;
-			break;
-		case 11:
-			//880-960 : 12 o'clock
-			hour = 12;
-			break;
-		}
-	
-	$("#time").text(hour);
-
-}
-
 function openClose (data) {
 	
 	if (data === 1){
